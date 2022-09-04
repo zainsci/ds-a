ee757fce7d149d572fb75a0151aae3e2bc7ab5df
zainsci <60652827+zainsci@users.noreply.github.com>
2022-09-04 09:19:00 +0500

    add gen_graph and use set instead of list in algo

--- algo/breadth-first/breadth-first.py
+++ algo/breadth-first/breadth-first.py
@@ -1,14 +1,31 @@
 #! /bin/python3
 
+def gen_graph(edges):
+    graph = {}
+
+    for edge in edges:
+        if edge[0] not in graph.keys():
+            graph[edge[0]] = [edge[1]]
+        else:
+            graph[edge[0]].append(edge[1])
+
+        if edge[1] not in graph.keys():
+            graph[edge[1]] = [edge[0]]
+        else:
+            graph[edge[1]].append(edge[0])
+
+    return graph
+
+
 def breadth_first(graph, source):
     queue = [source]
-    path = []
+    path = set()
 
     while len(queue) > 0:
         curr = queue.pop(0)
 
         if curr not in path:
-            path.append(curr)
+            path.add(curr)
 
         for i in graph[curr]:
             if i not in path:
@@ -40,6 +57,18 @@ def main():
         "h": ["g", "a", "b"]
     }
     path = breadth_first(graph, "a")
+    print(path)
+
+    edges = [
+        ["i", "j"],
+        ["k", "i"],
+        ["k", "j"],
+        ["m", "k"],
+        ["k", "l"],
+        ["o", "n"],
+    ]
+    graph = gen_graph(edges)
+    path = breadth_first(graph, "i")
     print(path)
 
 
--- algo/depth-first/depth-first.py
+++ algo/depth-first/depth-first.py
@@ -1,13 +1,30 @@
 #! /bin/python3
 
+def gen_graph(edges):
+    graph = {}
+
+    for edge in edges:
+        if edge[0] not in graph.keys():
+            graph[edge[0]] = [edge[1]]
+        else:
+            graph[edge[0]].append(edge[1])
+
+        if edge[1] not in graph.keys():
+            graph[edge[1]] = [edge[0]]
+        else:
+            graph[edge[1]].append(edge[0])
+
+    return graph
+
+
 def depth_first(graph, source):
     stack = [source]
-    path = []
+    path = set()
 
     while len(stack) > 0:
         curr = stack.pop()
         if curr not in path:
-            path.append(curr)
+            path.add(curr)
 
         for i in graph[curr]:
             if i not in path:
@@ -38,6 +55,18 @@ def main():
         "h": ["g", "a", "b"]
     }
     path = depth_first(graph, "a")
+    print(path)
+
+    edges = [
+        ["i", "j"],
+        ["k", "i"],
+        ["k", "j"],
+        ["m", "k"],
+        ["k", "l"],
+        ["o", "n"],
+    ]
+    graph = gen_graph(edges)
+    path = depth_first(graph, "i")
     print(path)
 
 
